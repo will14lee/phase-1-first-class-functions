b1fec74e5a2e5974d1a90227473a90ba3f31d634
will14lee <will14lee@gmail.com>
2021-06-09 13:29:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-
 // const dog=(function (){
 //     console.log("I'm Mary")
 // });
 
 // const dog=(function (){
 //   console.log("I'm Mary")
-// });
-// function receivesAFunction(spy) {
-//     spy();
-//   } 
+// // });
+function receivesAFunction(spy) {
+    spy();
+  } 
 // receivesAFunction(dog);
 
-
-var returnsANamedFunction= function fn() {};
-returnsANamedFunction.name;
-// var fn2=fn.name
-
-
-// function returnsANamedFunction(n=2) {
-//       console.log(n)
-//       if (n <= 1) {
-//         return 1;
-//       }
-//       return n * returnsANamedFunction(n);
-//     };
+function returnsANamedFunction() {
+  return function hello(){console.log("Hi!")
+}
+}
+console.log(returnsANamedFunction());
 function returnsAnAnonymousFunction(){
     return (()=>console.log("Y"));
 }  
-
-//fn= returnsAnAnonymousFunction()
-// done();
